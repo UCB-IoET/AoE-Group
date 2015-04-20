b19d190aa177c9a1ed1db782d6ced1ac6694ad6e
Nikita Kitaev <nikitakit@gmail.com>
2015-04-19 23:20:19 -0700

    Coffee svcd fixes

--- coffeepot/coffeepot.lua
+++ coffeepot/coffeepot.lua
@@ -18,6 +18,7 @@ function on()
     storm.io.set(1, relay)
     cord.await(storm.os.invokeLater, 300*storm.os.MILLISECOND)
     coffee_on = 1
+    storm.io.set(0, relay)
 end
 
 function beep(delay)
@@ -30,8 +31,9 @@ function write_status(msg, r, g, b)
     if r and g and b then
         lcd:setBackColor(r, g, b)
     end
+    lcd:clear()
     lcd:setCursor(0, 0)
-    lcd:writeString("STATUS:        ")
+    lcd:writeString("STATUS:")
     lcd:setCursor(1, 0)
     lcd:writeString(msg)
 end
@@ -40,7 +42,8 @@ function make_coffee(time)
     storm.os.cancel(announcer)
     on()
     beep()
-    local ret = "BREWING.." .. time or "BREWING COFFEE"
+    local seconds = time / storm.os.SECOND
+    local ret = "BREWING " .. (seconds / 60) .. ":" .. (seconds % 60)
     write_status(ret, 255, 192, 0)
 end
 
@@ -51,44 +54,22 @@ function notify_done()
     announcer = storm.os.invokePeriodically(20*storm.os.SECOND, SVCD.notify, COFFEE_SERVICE, MKCOFFEE_ATTR, "I love coffee!")
 end
 
-function init_svcd()
+function on_svcd_init()
+    -- Called when SVCD is ready to have services added
     SVCD.add_service(COFFEE_SERVICE)
-    SVCD.add_attr(COFFEE_SERVICE, MKCOFFEE_ATTR, function(payload, source_ip, source_port)
+    SVCD.add_attribute(COFFEE_SERVICE, MKCOFFEE_ATTR, function(payload, source_ip, source_port)
         cord.new(function()
             local args = storm.array.fromstr(payload)
             local time = args:get_as(storm.array.UINT16, 0)
-            make_coffee()
-            cord.await(storm.os.invokeLater, 17 * storm.os.SECOND + (time * 10) * storm.os.MILLISECOND)
+            time = 2 * 60 * storm.os.SECOND
+            make_coffee(time)
+            cord.await(storm.os.invokeLater, time)
             notify_done()
         end)
     end)
 end
 
-function init_bluetooth()
-    function onconnect(state)
-        if tmrhandle ~= nil then
-            storm.os.cancel(tmrhandle)
-            tmrhandle = nil
-        end
-        storm.os.invokePeriodically(1*storm.os.SECOND, function()
-            tmrhandle = storm.bl.notify(char_handle,
-            string.format("coffee: %d", coffee_on))
-        end)
-    end
-
-    storm.bl.enable("unused", onconnect, function()
-       local svc_handle = storm.bl.addservice(0x1245)
-       char_handle = storm.bl.addcharacteristic(svc_handle, 0x1246, function(x)
-            if x == "coffee: 1" then
-                on()
-                beep()
-                write_status("BREWING COFFEE", 255, 255, 0)
-            end
-       end)
-    end)
-end
-
-init_svcd()
+SVCD.init("coffee", on_svcd_init)
 
 cord.new(function ()
     lcd:init(2, 1)
